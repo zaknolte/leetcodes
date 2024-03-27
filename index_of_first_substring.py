c73d0656954b7bce5ceecec8691f7cb0b84df170
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-03-26 20:50:52 -0700

    find index of first substring in string

--- index_of_first_substring.py
+++ index_of_first_substring.py
@@ -3,6 +3,3 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         idx = re.search(f"({needle})", haystack)
         return idx.start(1) if idx is not None else -1
-
-a =Solution()
-print(a.strStr("as", "sad"))
